9a73075665b1bc8da10d12664dfd78693a72cc30
GiovannaMorais <gigimorais9@hotmail.com>
2022-04-14 15:42:55 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,10 +2,23 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  const colaboradora = data.employees.some((person) => person.managers.includes(id));
+  // return colaboradora;
+  return colaboradora;
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const listaDeColaboradores = data.employees.filter(
+    (persons) => persons.managers.includes(managerId),
+  );
+  const fullName = listaDeColaboradores.map(
+    (persons) => `${persons.firstName} ${persons.lastName}`,
+  );
+  return fullName;
 }
 
 module.exports = { isManager, getRelatedEmployees };
