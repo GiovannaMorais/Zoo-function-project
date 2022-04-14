fd4edda0a3d11d21736545765e6b141504295fe3
GiovannaMorais <gigimorais9@hotmail.com>
2022-04-14 15:12:04 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,8 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animals = data.species.find((specie) => specie.name === animal);
+  return animals.residents.every((idades) => idades.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
