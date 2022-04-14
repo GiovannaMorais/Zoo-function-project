2d1162e50912a37ca7e17198f69e79d8d225fd3f
GiovannaMorais <gigimorais9@hotmail.com>
2022-04-14 15:01:04 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const specieId = ids.map((id) => data.species.find((animal) => animal.id === id));
+  return specieId;
 }
 
 module.exports = getSpeciesByIds;
