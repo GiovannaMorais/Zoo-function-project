ab43a3499188e0129396e7e622ce2b4b041df9c6
GiovannaMorais <gigimorais9@hotmail.com>
2022-04-14 16:00:31 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
+// Se nenhum argumento for passado,
+//  retorna um objeto cujo o nome de cada espécie é uma chave desse objeto,
+// e o total de animais dessa espécie é o seu valor;
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const nothing = {};
+    data.species.forEach((animalAtual) => {
+      nothing[animalAtual.name] = animalAtual.residents.length;
+    });
+    return nothing;
+  }
+  if (animal.sex === undefined) {
+    return data.species.find(
+      (animalAtual) => animalAtual.name === animal.specie,
+    ).residents.length;
+  }
+  return data.species
+    .find((animalAtual) => animalAtual.name === animal.specie)
+    .residents.filter((animalAtual) => animalAtual.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
